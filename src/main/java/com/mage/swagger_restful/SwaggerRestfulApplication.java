66763ff9e6abdca6302ed8b8e9fedfccc0644936
Tony <kl2422@163.com>
2018-11-26 20:53:38 +0800

    init project

---
+++ src/main/java/com/mage/swagger_restful/SwaggerRestfulApplication.java
@@ -0,0 +1,12 @@
+package com.mage.swagger_restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerRestfulApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mage/swagger_restful/SwaggerRestfulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mage.swagger_restful;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SwaggerRestfulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
